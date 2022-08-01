56a64003816bc8614f90ff379586abf894fe998f
Jessica Nwaogbe <jessicanwaogbe96@gmail.com>
2022-07-31 20:26:26 -0400

    Updates

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,19 +1,20 @@
 var currentSlide = 1;
+showSlides(1);
 
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dotty");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {currentSlide = 1}
+  if (n < 1) {currentSlide = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[currentSlide-1].style.display = "block";
+  dots[currentSlide-1].className += " active";
   console.log(n)
 }
 
